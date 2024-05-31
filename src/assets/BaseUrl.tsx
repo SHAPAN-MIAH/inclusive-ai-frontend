d6981486b84d47a5d796b4c1020d8b310f324bf9
shapanmiah <shapanmiah19@gmail.com>
2024-05-31 15:57:49 +0600

    update

--- src/assets/BaseUrl.tsx
+++ src/assets/BaseUrl.tsx
@@ -1,2 +1,2 @@
-export const baseUrl = "http://server.inclusiveai.site/api/v1"
-// export const baseUrl = "http://localhost:8000/api/v1"
+// export const baseUrl = "http://server.inclusiveai.site/api/v1"
+export const baseUrl = "http://localhost:8000/api/v1"
